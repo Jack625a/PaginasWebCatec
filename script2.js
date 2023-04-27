58422286601293b82f3405f035125c9ea4e7e988
Jack625a <ktfut15@gmail.com>
2023-04-27 12:12:40 -0400

    Bucles While

---
+++ script2.js
@@ -0,0 +1,17 @@
+//Arrays
+Estudiantes={
+    Nombre:"Kevin",
+    Carrera: "Sistemas Informaticos",
+    Ci:"84541561"
+}
+//document.write(Estudiante["Carrera"])
+//Bucles - Ciclos (While - For -do while)
+//Bucle While
+
+let numero=0
+while(numero<10){
+    numero++;
+    document.write(numero + "<br>")
+}
+
+
