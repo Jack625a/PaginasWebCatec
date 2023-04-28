5594f8cefa113f134c5a947122dc3f3d11309332
Jack625a <ktfut15@gmail.com>
2023-04-28 11:19:33 -0400

    Ejercicio2

--- condicionales.js
+++ condicionales.js
@@ -6,3 +6,15 @@ while(contador<=100){
     }
     contador ++
 }
+//Ejercicio 2 - Mostrar los numeros pares del 1 al N (N sera ingresado por el usuario por teclado)
+document.write("<br>")
+document.write("<h1>EJERCICIO 2</h1>")
+let contador2=1
+let n=prompt("Ingrese el valor final de los numeros pares que desea ver")
+while(contador2<=n){
+    if (contador2 %2==0){
+        document.write(contador2+"<br>")
+    }
+    contador2++
+}
+
