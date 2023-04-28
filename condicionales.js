96dad5be1ecca7186e6e1aac7d0e73bd1d2c7d52
Jack625a <ktfut15@gmail.com>
2023-04-28 11:00:49 -0400

    CondicionalesNumeros Pares

---
+++ condicionales.js
@@ -0,0 +1,8 @@
+//Ejercicio 1 - mostrar los numeros pares del 1 al 100
+let contador=1
+while(contador<=100){
+    if (contador %2==0){
+        document.write(contador+"<br>")
+    }
+    contador ++
+}
