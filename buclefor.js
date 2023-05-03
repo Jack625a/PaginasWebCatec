26fd36c7f8f0caf4f2d404b8413cac5b075e65f8
limberth01 <limberthcopa9@gmail.com>
2023-05-03 12:02:58 -0400

    tabla de multiplicar

--- buclefor.js
+++ buclefor.js
@@ -27,3 +27,26 @@ for(let contador=1; contador<=100;contador++){
 //mostrar la secuencia del 100 al 1
 document.write("<br>")
 document.write("<h1>Ejercicio 4 - FOR</h1>")
+for(let i=100; i>=1; i--){
+    document.write(i+"<br>")
+}
+//mostrar la secuencia del 100 al 1
+document.write("<br>")
+document.write("<h1>Ejercicio 5 FOR</h1>")
+let multiplicar=5
+document.write("tabla de multiplicar del "+multiplicar)
+document.write("<br>")
+for(let cont=1;cont<=10;cont++){
+    resultado=multiplicar*cont
+    document.write(multiplicar+" x "+cont+" = "+resultado+"<br>")
+}
+//mostrar la secuencia del 100 al 1
+document.write("<br>")
+document.write("<h1>Ejercicio 6 - FOR</h1>")
+let multi=parseInt(prompt("ingrese el numero de la tabla de multiplicar de desea: "))
+document.write("tabla de multiplicar del "+multi)
+document.write("<br>")
+for(let cont=1;cont<=10;cont++){
+    resultado=multi*cont
+    document.write(multi+" x "+cont+" = "+resultado+"<br>")
+}
