c092772181cb1f651e0ef485e075909f1212b5e0
romernina <romerninap@gmail.com>
2023-05-11 08:30:50 -0400

    tipos de datos javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+Materia="Paginas Web I-Sistemas Informaticos"
+//tipos de variables
+var nombre="Romer Nina"
+nombre="juan"
+let edad=26
+const pi=3.141658
+//pi=25 no se altera porque es constante
+//TIPOS DE DATOS
+//string-cadenas de caracteres
+var apellido="Poma"
+var numero0="A45"
+//numericos-Int, Doubles
+var numero1=45
+var numero2=12.4
+//booleanos true false
+var encender=true
+//casos especiales
+//tipos de datos no definidos
+let celular
+
+//tipos de datos nulos
+carnet=null
+     /*alert(carnet)*/
+//tipos de datos NaN
+     /*alert(numero0*numero1)*/
+//funcion prompt
+let nombres=prompt("ingrese su nombre")
+let correo=prompt("ingrese su correo")
+alert("hola bienvenido "+nombres+"  correo es:"+correo)
+//operadores en javascript
