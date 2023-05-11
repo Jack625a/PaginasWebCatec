4982f6cbdaadfd581bba8ad80749f44e3e198ebf
romernina <romerninap@gmail.com>
2023-05-11 14:53:44 -0400

    creacion de script2.js

---
+++ script2.js
@@ -0,0 +1,35 @@
+//arrays
+Estudiante=
+{
+  Nombre:"Romer",
+  Carrera:"Sistemas Informaticos",
+  CI:"789654"
+}
+//document.write(Estudiante["Carrera"])
+//bucles-ciclos for while dowhile
+//bucle while
+let numero=0
+while(numero<10)
+{
+    numero++
+    document.write(numero+"<br>")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
