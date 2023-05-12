593113d43d41e240b58ff1d200f3f8374a44b5d9
romernina <romerninap@gmail.com>
2023-05-12 08:50:54 -0400

    switch2

--- condicionJs.js
+++ condicionJs.js
@@ -47,3 +47,70 @@ alert("producto agregado correctamente")
 document.write("<br>el precio es: "+precio+" Bs")
 document.write("<br>la cantidad seleccionada es: "+cantidad)
 document.write("<br>el precio totalfruta es: "+precioT+" Bs")
+
+productos=prompt("desea agregar mas frutas? si/no")
+if(productos=="si")
+{
+    switch(fruta)
+    {
+        case "manzana":
+        precio=2.5
+        break;
+        case "naranja":
+        precio=1.5
+        break;
+        case "frutilla":
+        precio=3.5
+        break;
+        case "pera":
+        precio=1.5
+        break;
+        case "sandia":
+        precio=8
+        break;
+        default:
+        precio="no se encuentra"
+    }
+    cantidad=parseInt(prompt("ingrese cantidad de fruta"))
+    precioT=precio*cantidad
+    alert("producto agregado correctamente")
+
+}
+else
+{
+    document.write("<br>el precio es: "+precio+" Bs")
+    document.write("<br>la cantidad seleccionada es: "+cantidad)
+    document.write("<br>el precio totalfruta es: "+precioT+" Bs")
+}
+
+
+//Con el Bucle While 
+let productoss=prompt("Desea seleccionar mas frutas, si /no")
+while(productoss=="si"){
+    let frutas=prompt("Seleccione la fruta que desea: manzana, naranja, frutilla, pera, sandia ")
+    var precio
+    switch (frutas){
+        case "manzana":
+        precio=2.5 
+        break
+        case "naranja":
+        precio=1.5
+        break
+        case "frutilla":
+        precio=3.5
+        break
+        case "pera":
+        precio=1.80
+        break
+        case "sandia":
+        precio=8
+        break
+        default:
+        precio="El producto no se encuentra en stock"
+    }
+cantidad=parseInt(prompt("Ingrese la cantidad de fruta que desea"))
+precioTotal=precio*cantidad
+precioFinal+=precioTotal
+alert("Producto agregadado correctamente")
+let productoss=prompt("Desea seleccionar mas frutas, si /no")
+}
